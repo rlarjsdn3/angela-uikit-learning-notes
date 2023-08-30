d0f9047de144db09f7008a92fa599176b0ae7a02
Zizi_Kim <rlarjsdn3@naver.com>
2023-08-30 14:34:57 +0900

    I am Rich 프로젝트 추가

---
+++ 01-I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 김건우 on 2023/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
