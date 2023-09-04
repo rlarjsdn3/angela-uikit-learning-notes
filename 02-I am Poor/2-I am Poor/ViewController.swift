4e9da3f53199657e9aeecb3aca72e9b9e53ca33f
Zizi_Kim <rlarjsdn3@naver.com>
2023-09-04 19:29:24 +0900

    02-I am Poor 프로젝트 추가

---
+++ 02-I am Poor/2-I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2-I am Poor
+//
+//  Created by 김건우 on 2023/09/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
