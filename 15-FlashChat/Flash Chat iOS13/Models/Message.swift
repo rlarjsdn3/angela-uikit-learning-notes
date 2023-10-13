6449dc19ad83ac7170d9b59b8e16348a20d6ced0
Zizi_Kim <rlarjsdn3@naver.com>
2023-10-13 20:02:08 +0900

    15-FlashChat 프로젝트 추가

---
+++ 15-FlashChat/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 김건우 on 10/13/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    
+}
---
+++ 15-FlashChat/Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by 김건우 on 10/13/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 4.0
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
